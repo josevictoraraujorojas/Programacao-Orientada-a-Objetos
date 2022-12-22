fd55fe980c0e62bcf825447b3b12467aeb9bb9e1
josevictoraraujorojas <kaneki1212112121@gmail.com>
2022-12-21 23:51:22 -0300

    Objeto e mensagem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Carro carro = new Carro();
+
+        carro.setCor("vermelho");
+        carro.setModelo("uno");
+        carro.setCapacidadeDoTanque(12);
+
+        System.out.printf("Cor do carro:%s \nModelo do Carro:%s\nCapacidade do tanque do carro:%d\n",carro.getCor(),carro.getModelo(),carro.getCapacidadeDoTanque());
+
+        System.out.printf("Custo da capacidade total do tanque do carro:%f",carro.totalValorTanque(4));
     }
 }
